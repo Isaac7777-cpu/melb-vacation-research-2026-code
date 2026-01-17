f4d4cb40defe6857d2e1d1072693e715a0f596d7
Isaac Leong <isaacleong23367@gmail.com>
2026-01-17 14:42:48 +1100

    Feat: Added Code for Getting Better Understanding Why phi is off

--- src/analyse_simulation.R
+++ src/analyse_simulation.R
@@ -25,3 +25,24 @@ sqrt(mean((multi_resolution$true_phi - multi_resolution$theta[, 1])**2))
 # [1] 0.6383653
 sqrt(mean((multi_resolution$true_sigmasq - multi_resolution$theta[, 2])**2))
 # [1] 0.1761742
+
+# Looking Deeper for phi -------------------------------------------------------
+hist(x = multi_resolution$theta[, 1])
+
+# Bias
+bias <- mean(multi_resolution$theta[, 1] - multi_resolution$true_phi)
+# [1] -0.06827891
+mean(multi_resolution$theta[, 2] - multi_resolution$true_sigmasq)
+# [1] -0.02838421
+mean(multi_resolution$alpha0 - multi_resolution$true_alpha0)
+# [1] 0.006571065
+
+# Variance
+var <- var(multi_resolution$theta[, 1])
+# [1] 0.4036555
+var(multi_resolution$theta[, 2])
+# [1] 0.03029228
+var(multi_resolution$alpha0)
+# [1] 0.02794304
+
+# sqrt(bias**2 + var) # This should be the MSE
