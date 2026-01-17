da54783d7193d2e27671ed3d3d263973f4e6f181
Isaac Leong <isaacleong23367@gmail.com>
2026-01-17 14:14:07 +1100

    Removed unused run_simulation file

--- src/run_simulation.R
+++
@@ -1,6 +0,0 @@
-# Source the required libraries ------------------------------------------------
-source(file = "src/likelihood.R", verbose = TRUE)
-source(file = "src/sample_from_grid.R", verbose = TRUE)
-
-
-
