6cb28b2912fe3c80e1e59f4d1a3f47c00ff384af
Isaac Leong <isaacleong23367@gmail.com>
2026-02-02 00:48:17 +1100

    Updated the Implementation to Kriging

--- src/run_kriging.R
+++ src/run_kriging.R
@@ -4,17 +4,35 @@ source(file = "src/kriging.R", verbose = TRUE)
 sim_res <- readRDS(file = "data/simulation_res_reml.rds")
 # multi_resolution <- readRDS(file = "data/x_simulation.rds")
 
-slice_idx <- 2
-target_res <- 5
-request_coord <- c(x = 14, y = 92)
-# If you want random coordinate for the 100x100 grid domain
-# request_coord <- sample.int(size = 2, n = 98, replace = TRUE) + 1
+slice_idx <- 1
+target_res <- as.integer(5)
+request_coord <- matrix(data = c(x = c(14, 21), y = c(94, 10)), ncol = 2)
 
+stopifnot(target_res >= 1L, target_res %% 2L == 1L) # odd block size
 alpha0 <- sim_res$alpha0[slice_idx]
 phi <- sim_res$theta[slice_idx, 1]
 sigmasq <- sim_res$theta[slice_idx, 2]
 coord <- sim_res$x.coord[slice_idx, , ]
 obs <- sim_res$x_obs[slice_idx, ]
+
+# Compute target coordinates
+target_half <- target_res %/% 2L
+target_offsets <- expand.grid(
+  dx = seq(-target_half, target_half),
+  dy = seq(-target_half, target_half)
+)
+target_size <- dim(request_coord)[1]
+coord.target.box <- array(
+  dim = c(target_size, target_res**2, 2),
+  dimnames = list(
+    point = NULL,
+    cell = NULL,
+    coord = c("x", "y")
+  )
+)
+for (p in seq_len(target_size)) {
+  coord.target.box[p, , ] <- t(request_coord[p, ] + t(target_offsets))
+}
 
 # Get the distance tensor
 src_res <- sim_res$resolution
@@ -23,7 +41,7 @@ src_offsets <- expand.grid(
   dx = seq(from = -half, to = half, by = 1),
   dy = seq(from = -half, to = half, by = 1)
 )
-coord.box <- array(
+coord.src.box <- array(
   dim = c(sim_res$npoints, src_res**2, 2),
   dimnames = list(
     point = NULL,
@@ -31,23 +49,41 @@ coord.box <- array(
     coord = c("x", "y")
   )
 )
-for (p in seq_len(400)) {
-  coord.box[p, , ] <- t(coord[, p] + t(src_offsets))
+for (p in seq_len(sim_res$npoints)) {
+  coord.src.box[p, , ] <- t(coord[, p] + t(src_offsets))
 }
 
 # Reduce to the distance
-dx <- coord.box[,, "x", drop = FALSE] - request_coord[1]
-dy <- coord.box[,, "y", drop = FALSE] - request_coord[2]
-coord.dist <- sqrt(dx**2 + dy**2)
+
+m <- dim(coord.src.box)[1]
+s <- dim(coord.src.box)[2]
+n_targets <- dim(coord.target.box)[1]
+k <- dim(coord.target.box)[2]
+
+src_xy <- matrix(coord.src.box, ncol = 2) # Flatten the first two dimensions
+tgt_xy <- matrix(coord.target.box, ncol = 2) # Flatten the first two dimensions
+
+dx_mat <- outer(src_xy[, 1], tgt_xy[, 1], "-")
+dy_mat <- outer(src_xy[, 2], tgt_xy[, 2], "-")
+
+dx_raw <- array(dx_mat, dim = c(m, s, n_targets, k))
+dy_raw <- array(dy_mat, dim = c(m, s, n_targets, k))
+
+dx <- aperm(dx_raw, perm = c(1, 3, 4, 2)) # (m,t,k,s)
+dy <- aperm(dy_raw, perm = c(1, 3, 4, 2)) # (m,t,k,s)
+
+dist <- sqrt(dx**2 + dy**2) # (m,t,k,s)
 
 # Obtain the covariance vector
 coord.cov <- cov.exponential(
-  dist = coord.dist,
+  dist = dist,
   sigmasq = sigmasq,
   phi = phi,
   tausq = NA
-)
-c <- rowMeans(x = coord.cov)
+) # (m,t,k,s)
+
+# Average over the neighbour dim (k, s)
+c <- apply(coord.cov, c(1, 2), mean) # (m, t)
 
 # Retrieved the covariance matrix
 m <- sim_res$npoints
@@ -72,12 +108,6 @@ obs.cov.full <- sigmasq *
     eta = c(phi, sigmasq),
     nug = FALSE
   )
-obs.cov.full <- cor.mat(
-  D = pairwise_dist,
-  cov_model = "Exp",
-  eta = c(phi),
-  nug = FALSE
-)
 resolution_matrix <- kronecker(
   diag(m),
   matrix(rep(1 / src_res**2, src_res**2), nrow = 1)
@@ -92,24 +122,34 @@ blup <- alpha0 + crossprod(c, quad_term)
 # blup <- drop(blup) # Turn to a single scalar
 
 # Compute the optimiser
-one <- rep(1, m)
-Vinv_c <- backsolve(t(L), forwardsolve(L, c))
-Vinv_one <- backsolve(t(L), forwardsolve(L, one))
-nu <- drop((1 - crossprod(one, Vinv_c)) / crossprod(one, Vinv_one))
-lambda <- Vinv_c + nu * Vinv_one
+one <- rep(1, m) # m
+Vinv_one <- backsolve(t(L), forwardsolve(L, one)) # m
+Vinv_c <- backsolve(t(L), forwardsolve(L, c)) # (m,t)
+nu <- drop(1 - crossprod(one, Vinv_c)) / drop(crossprod(one, Vinv_one)) # t
+lambda <- Vinv_c + Vinv_one %*% t(nu) # m
+blup_check <- crossprod(lambda, obs)
+
+# To check for correctness of the code, we can check that lambda^T %*% obs
+# is indeed the BLUP we calculated using the simplified formula above.
+stopifnot(
+  "lambda^top %*% obs != BLUP" = all.equal(
+    drop(blup),
+    drop(blup_check),
+    tolerance = 1e-10
+  )
+)
+
 # Check if they are the same
 Vinv_obs <- backsolve(t(L), forwardsolve(L, obs))
 alpha0_gls <- drop(crossprod(one, Vinv_obs) / crossprod(one, Vinv_one))
 
 stopifnot(
   "The sum of lambda should be 1 to satisfy the constraint" = all.equal(
-    sum(lambda),
-    1.0,
+    drop(colSums(lambda)),
+    rep(1, n_targets),
     tolerance = 1e-10
   )
 )
-
-alpha0 - alpha0_gls
 
 stopifnot(
   "Recalculated alpha0 should be the same as the one loaded" = all.equal(
@@ -120,13 +160,19 @@ stopifnot(
 )
 
 # Compute the BLUP variance
-blup_var <- drop(
-  cov.exponential(
-    dist = 0,
-    phi = phi,
-    sigmasq = sigmasq,
-    tausq = NA
-  ) -
-    crossprod(c, Vinv_c) +
-    nu**2 * crossprod(one, Vinv_one)
+k <- dim(coord.target.box)[2]
+w <- rep(1 / k, k)
+
+D_p <- as.matrix(dist(as.matrix(target_offsets)))
+Sigma_p <- cov.exponential(
+  dist = D_p,
+  phi = phi,
+  sigmasq = sigmasq,
+  tausq = NA
 )
+block_var <- drop(crossprod(w, Sigma_p %*% w))
+
+# term2 <- diag(t(c) %*% Vinv_c)
+term2 <- colSums(c * Vinv_c)
+
+blup_var <- block_var - term2 + drop(crossprod(one, Vinv_one)) * (nu**2)
