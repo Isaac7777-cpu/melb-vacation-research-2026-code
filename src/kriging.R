777dfbc44bc7b57e5ed26d9e3554fa339af29449
Isaac Leong <isaacleong23367@gmail.com>
2026-02-03 18:01:24 +1100

    Updated Kriging Scripts to not Depend on Parity

--- src/kriging.R
+++ src/kriging.R
@@ -1,3 +1,4 @@
+library(progress)
 source(file = "lib/BFuns.R", verbose = TRUE)
 
 #' Calculate the variance
@@ -20,10 +21,20 @@ ordinary.kriging <- function(
   phi
 ) {
   # Compute target coordinates
+  cat("[COV-COMP 1/3]: Building target coordiante block tensor...\n")
   target_half <- target_res %/% 2L
+  target_parity_correction <- ((target_res + 1) %% 2) / 2
   target_offsets <- expand.grid(
-    dx = seq(-target_half, target_half),
-    dy = seq(-target_half, target_half)
+    dx = seq(
+      -target_half + target_parity_correction,
+      target_half - target_parity_correction,
+      by = 1
+    ),
+    dy = seq(
+      -target_half + target_parity_correction,
+      target_half - target_parity_correction,
+      by = 1
+    )
   )
   target_size <- dim(request_coord)[1]
   coord.target.box <- array(
@@ -38,11 +49,21 @@ ordinary.kriging <- function(
     coord.target.box[p, , ] <- t(request_coord[p, ] + t(target_offsets))
   }
 
-  # Get the distance tensor
+  # Get the source box coordinate tensor
+  cat("[COV-COMP 2/3]: Building source coordiante block tensor...\n")
   half <- src_res %/% 2
+  src_parity_corrention <- ((src_res + 1) %% 2) / 2
   src_offsets <- expand.grid(
-    dx = seq(from = -half, to = half, by = 1),
-    dy = seq(from = -half, to = half, by = 1)
+    dx = seq(
+      from = -half + src_parity_corrention,
+      to = half - src_parity_corrention,
+      by = 1
+    ),
+    dy = seq(
+      from = -half + src_parity_corrention,
+      to = half - src_parity_corrention,
+      by = 1
+    )
   )
   coord.src.box <- array(
     # dim = c(sim_res$npoints, src_res**2, 2),
@@ -58,39 +79,48 @@ ordinary.kriging <- function(
   }
 
   # Reduce to the distance
-
+  cat("[COV-COMP 3/3]: Computing the covariance matrix...\n")
   m <- dim(coord.src.box)[1]
   s <- dim(coord.src.box)[2]
-  n_targets <- dim(coord.target.box)[1]
+  n_targets <- dim(coord.target.box)[1] # which is t, avoid shadowing
   k <- dim(coord.target.box)[2]
 
-  src_xy <- matrix(coord.src.box, ncol = 2) # Flatten the first two dimensions
-  tgt_xy <- matrix(coord.target.box, ncol = 2) # Flatten the first two dimensions
+  c <- matrix(data = NA, nrow = m, ncol = n_targets)
 
-  dx_mat <- outer(src_xy[, 1], tgt_xy[, 1], "-")
-  dy_mat <- outer(src_xy[, 2], tgt_xy[, 2], "-")
+  pb <- progress_bar$new(
+    format = "  [:bar] :percent | elapsed: :elapsed | ETA: :eta",
+    total = m * n_targets,
+    clear = FALSE,
+    width = 60
+  )
 
-  dx_raw <- array(dx_mat, dim = c(m, s, n_targets, k))
-  dy_raw <- array(dy_mat, dim = c(m, s, n_targets, k))
+  for (tp in seq_len(n_targets)) {
+    tc.box.flatten <- coord.target.box[tp, , ] # (k, 2)
+    a2 <- rowSums(tc.box.flatten * tc.box.flatten) # [k]
 
-  dx <- aperm(dx_raw, perm = c(1, 3, 4, 2)) # (m,t,k,s)
-  dy <- aperm(dy_raw, perm = c(1, 3, 4, 2)) # (m,t,k,s)
+    for (sp in seq_len(m)) {
+      # Get the requesting coordinate box
+      sc.box.flatten <- coord.src.box[sp, , ] # (s, 2)
+      b2 <- rowSums(sc.box.flatten * sc.box.flatten) # [s]
 
-  dist <- sqrt(dx**2 + dy**2) # (m,t,k,s)
+      dist <- sqrt(
+        outer(a2, b2, "+") - 2 * tcrossprod(tc.box.flatten, sc.box.flatten)
+      ) # (k, s)
 
-  # Obtain the covariance vector
-  coord.cov <- cov.exponential(
-    dist = dist,
-    sigmasq = sigmasq,
-    phi = phi,
-    tausq = NA
-  ) # (m,t,k,s)
+      cov.vec <- cov.exponential(
+        dist = dist,
+        sigmasq = sigmasq,
+        phi = phi,
+        tausq = NA
+      ) # (k, s)
 
-  # Average over the neighbour dim (k, s)
-  c <- apply(coord.cov, c(1, 2), mean) # (m, t)
+      c[sp, tp] <- mean(cov.vec)
+
+      pb$tick()
+    }
+  }
 
   # Retrieved the covariance matrix
-  # m <- sim_res$npoints
   m <- length(obs)
   stopifnot(
     "The coordinate vector should be of size 2 x npoints" = m == ncol(coord)
@@ -103,7 +133,7 @@ ordinary.kriging <- function(
   }
 
   cat(sprintf("The following should form %dx%d block\n", src_res, src_res))
-  print(fine_coords[, 1:9])
+  print(fine_coords[, 1:src_res**2])
 
   pairwise_dist <- as.matrix(dist(t(fine_coords)))
   obs.cov.full <- sigmasq *
@@ -120,6 +150,7 @@ ordinary.kriging <- function(
   obs.cov <- resolution_matrix %*% obs.cov.full %*% t(resolution_matrix)
 
   # Compute the BLUP
+  cat("[BLUP-ANALYSIS 1/4]: Computing the blup...\n")
   L <- t(chol(obs.cov))
   r <- obs - rep(alpha0, m)
   quad_term <- backsolve(r = t(L), x = forwardsolve(l = L, x = r))
@@ -127,6 +158,7 @@ ordinary.kriging <- function(
   # blup <- drop(blup) # Turn to a single scalar
 
   # Compute the optimiser
+  cat("[BLUP-ANALYSIS 2/4]: Computing the primal and dual optimiser...\n")
   one <- rep(1, m) # m
   Vinv_one <- backsolve(t(L), forwardsolve(L, one)) # m
   Vinv_c <- backsolve(t(L), forwardsolve(L, c)) # (m,t)
@@ -134,6 +166,7 @@ ordinary.kriging <- function(
   lambda <- Vinv_c + Vinv_one %*% t(nu) # m
   blup_check <- crossprod(lambda, obs)
 
+  cat("[BLUP-ANALYSIS 3/4]: Verifying BLUP results...(3 checks in total)\n")
   # To check for correctness of the code, we can check that lambda^T %*% obs
   # is indeed the BLUP we calculated using the simplified formula above.
   stopifnot(
@@ -143,11 +176,10 @@ ordinary.kriging <- function(
       tolerance = 1e-10
     )
   )
-
+  cat("[BLUP-ANALYSIS 3/4]:   ✓ Analytical BLUP calculation correct.\n")
   # Check if they are the same
   Vinv_obs <- backsolve(t(L), forwardsolve(L, obs))
   alpha0_gls <- drop(crossprod(one, Vinv_obs) / crossprod(one, Vinv_one))
-
   stopifnot(
     "The sum of lambda should be 1 to satisfy the constraint" = all.equal(
       drop(colSums(lambda)),
@@ -155,7 +187,7 @@ ordinary.kriging <- function(
       tolerance = 1e-10
     )
   )
-
+  cat("[BLUP-ANALYSIS 3/4]:   ✓ Primal feasibility checked.\n")
   stopifnot(
     "Recalculated alpha0 should be the same as the one loaded" = all.equal(
       unname(alpha0),
@@ -163,11 +195,12 @@ ordinary.kriging <- function(
       tolerance = 1e-10
     )
   )
+  cat("[BLUP-ANALYSIS 3/4]:   ✓ Reconstruction of variance matrix matched.\n")
 
   # Compute the BLUP variance
+  cat("[BLUP-ANALYSIS 4/4]: Computing the objective / BLUP variance...\n")
   k <- dim(coord.target.box)[2]
   w <- rep(1 / k, k)
-
   D_p <- as.matrix(dist(as.matrix(target_offsets)))
   Sigma_p <- cov.exponential(
     dist = D_p,
@@ -176,15 +209,13 @@ ordinary.kriging <- function(
     tausq = NA
   )
   block_var <- drop(crossprod(w, Sigma_p %*% w))
-
-  # term2 <- diag(t(c) %*% Vinv_c)
   term2 <- colSums(c * Vinv_c)
-
   blup_var <- block_var - term2 + drop(crossprod(one, Vinv_one)) * (nu**2)
 
+  cat("[KRIGING]: Kriging Completed!\n")
   list(
     nu = nu,
-    lambda = lambda,
+    lambda = t(lambda), # Transpose so that it is (t, m) instead of (m, t)
     blup = drop(blup),
     blup_var = drop(blup_var)
   )
